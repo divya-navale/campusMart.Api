a0303289f7388f015b0b93eb17c5f7aa00e7b2de
Divya Navale <navad01@pfw.edu>
2024-11-17 17:17:04 -0500

    added nodemon and updated server file

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-const app = require('./app');
+const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
+require('dotenv').config();
+
+const app = express();
+
+const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use('/api', productRoutes);
+app.use('/api', userRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
@@ -18,3 +31,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = app;
