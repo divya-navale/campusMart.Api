b651fd4f25de56af4590c32824402eaf6a4f3cef
divya-navale <navad01@pfw.edu>
2024-12-05 19:01:42 -0500

    update password code

--- controllers/userController.js
+++ controllers/userController.js
@@ -126,6 +126,25 @@ const logoutUser = async (req, res) => {
     .json({ message: 'Logged out successfully' });
 };
 
+const updatePassword = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+    res.status(200).json({ message: 'Password updated successfully' });
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to update password', error: err.message });
+  }
+};
+
 module.exports = {
   addUser,
   deleteUser,
@@ -133,4 +152,5 @@ module.exports = {
   getUser,
   verifyUser,
   logoutUser,
+  updatePassword
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { addUser, deleteUser, updateUser, getUser, verifyUser, logoutUser } = require('../controllers/userController');
+const { addUser, deleteUser, updateUser, getUser, verifyUser, logoutUser, updatePassword } = require('../controllers/userController');
 const authMiddleware = require('./../middleware/authMiddleware')
 
 router.post('/users', addUser);
 router.delete('/users/:id', authMiddleware, deleteUser);
 router.put('/users/:id', authMiddleware, updateUser);
 router.get('/users/:id', authMiddleware, getUser);
-router.get('/users/email/:email', authMiddleware, getUser);
+router.get('/users/email/:email', getUser);
 router.post('/users/verify', verifyUser);
 router.post('/users/logout', authMiddleware, logoutUser); 
+router.post('/update-password', updatePassword);
 
 module.exports = router;
