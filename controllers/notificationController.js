6d4aed6e6078535f29017999e04758d46b72959d
divya-navale <navad01@pfw.edu>
2024-12-08 21:12:53 -0500

    change

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,4 +1,4 @@
-const Notification = require('../models/Notification');
+const Notification = require('../models/notification');
 
 // Create a notification
 exports.createNotification = async (req, res) => {
