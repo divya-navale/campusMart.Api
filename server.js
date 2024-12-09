2b453c057f9f45887b7fc84689a509772bb623a1
divya-navale <navad01@pfw.edu>
2024-12-08 21:21:32 -0500

    co

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ app.use(cors({
   credentials: true,
 }));
 
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 app.use('/api', productRoutes);
 app.use('/api', userRoutes);
